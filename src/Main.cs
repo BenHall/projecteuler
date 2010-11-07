becdc7b7398bf08f7758b38d1595c7be81f6ce1d
BenHall <Ben@Benhall.me.uk>
2010-11-07 23:39:45 +0000

    Outline for problem 6

--- src/Main.cs
+++ src/Main.cs
@@ -7,7 +7,7 @@ namespace src
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
-			new Problem5().Solve();
+			new Problem6().Solve();
 		}
 	}
 }
---
+++ src/Problem6.cs
@@ -0,0 +1,24 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace src
+{
+//The sum of the squares of the first ten natural numbers is,
+
+//12 + 22 + ... + 102 = 385
+//The square of the sum of the first ten natural numbers is,
+
+//(1 + 2 + ... + 10)2 = 552 = 3025
+//Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
+
+//Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+	public class Problem6
+	{
+		public void Solve ()
+		{
+			int answer = 0;	
+			Console.WriteLine("Answer: " + answer);
+		}
+	}
+}
